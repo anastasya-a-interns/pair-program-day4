4324ff6af2843d4e24c5d7ed9a5f02b75075f0fb
anastasya.a.interns <anastasya.a.interns@aux.gojek.com>
2021-09-23 16:22:15 +0700

    feat: add new test case to compare line when all points are same & create new compareLine method

--- lib/src/test/java/com/minibootcamp/pasti/LineTest.java
+++ lib/src/test/java/com/minibootcamp/pasti/LineTest.java
@@ -1,6 +1,7 @@
 package com.minibootcamp.pasti;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -16,5 +17,12 @@ class LineTest {
     void getLength_ShouldReturnValueMoreThanZero_WhenAtLeastOneInputIsDifferent() {
         Line line = new Line(2, 1, 1, 1);
         assertEquals(1, line.getLength(), 0);
+    }
+
+    @Test
+    void compareLine_ShouldEqualTrue_WhenPointsAreSame() {
+        Line line1 = new Line(1, 2, 3, 4);
+        Line line2 = new Line(1, 2, 3, 4);
+        assertTrue(line1.compareLine(line2));
     }
 }
