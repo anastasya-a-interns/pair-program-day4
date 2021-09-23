8135d521a15d3a6f4e87db4a0281021341f68835
anastasya.a.interns <anastasya.a.interns@aux.gojek.com>
2021-09-23 16:18:15 +0700

    feat: add test case to return value more than zero if at least one line point is different

--- lib/src/test/java/com/minibootcamp/pasti/LineTest.java
+++ lib/src/test/java/com/minibootcamp/pasti/LineTest.java
@@ -11,4 +11,10 @@ class LineTest {
         Line line = new Line(1, 1, 1, 1);
         assertEquals(0, line.getLength(), 0);
     }
+
+    @Test
+    void getLength_ShouldReturnValueMoreThanZero_WhenAtLeastOneInputIsDifferent() {
+        Line line = new Line(2, 1, 1, 1);
+        assertEquals(1, line.getLength(), 0);
+    }
 }
